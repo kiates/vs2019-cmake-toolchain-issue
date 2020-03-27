bc517703d31eb9f788a1558a7eb57ca9fc2faccc
Chad Yates <chad_yates@selinc.com>
2020-03-27 10:52:19 -0700

    Initial

---
+++ contrived-sdk/sysroot/include/custom-header.h
@@ -0,0 +1,6 @@
+#include <string>
+
+std::string sdk_arch_function()
+{
+	return "Hello World from a function!";
+}
---
+++ main.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+#include <custom-header.h>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	cout << sdk_arch_function() << endl;
+	return 0;
+}
